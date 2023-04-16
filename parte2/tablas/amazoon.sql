b9e2e321e41a277bec0734ff03929256d4229835
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2023-04-16 13:53:16 +0200

    dblink parte 2 (sin pruebas)

--- parte2/tablas/amazoon.sql
+++ parte2/tablas/amazoon.sql
@@ -13,23 +13,15 @@ CREATE TABLE POS (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE magazines (
+CREATE TABLE products (
   INTL              VARCHAR(36),
   title             VARCHAR(150)  NOT NULL,
   publicationDate   DATE          NOT NULL,
-  PRIMARY KEY (INTL)
-);
-
-CREATE TABLE books (
-  INTL              VARCHAR(36),
-  title             VARCHAR(150)  NOT NULL,
-  publicationDate   DATE          NOT NULL,
+  type              VARCHAR(8)    NOT NULL  CHECK(type IN ('magazine', 'book')),
   edition           VARCHAR(255)  NOT NULL,
   author            VARCHAR(255)  NOT NULL,
   PRIMARY KEY (INTL)
 );
-
--- PEDIENTE LAS DE ABAJO 
 
 CREATE TABLE buys (
   DNI                       VARCHAR(255),
@@ -39,10 +31,10 @@ CREATE TABLE buys (
   shippingAddr              VARCHAR(100)      NOT NULL,
   PRIMARY KEY (DNI, INTL),
   FOREIGN KEY (DNI)         REFERENCES clients (DNI),
-  FOREIGN KEY (INTL)        REFERENCES magazines (INTL)
+  FOREIGN KEY (INTL)        REFERENCES products (INTL)
 );
 
-CREATE TABLE storage (
+CREATE TABLE stores (
   idPOS     VARCHAR(255),
   INTL      VARCHAR(255),
   Quantity  INT(11) NOT NULL,
--- parte2/tablas/global.sql
+++ parte2/tablas/global.sql
@@ -1,30 +1,50 @@
-CREATE VIEW clients AS (
-    SELECT DNI, name, email, billingAddr
-    FROM clients;
-)
-CREATE VIEW POS AS (
-    SELECT id, type, address
-    FROM POS;
-)
-CREATE VIEW magazines AS (
-    SELECT INTL, title, publicationDate
-    FROM magazines;
-    -- falta meter la relación sells
-)
-CREATE VIEW books AS (
-    SELECT INTL, title, publicationDate, edition, author
-    FROM books;
-    -- falta meter la relación sells
-)
-CREATE VIEW worker AS (
-    SELECT DNI, nombre, genero, salario, fechaIncorporacion, fechaSalida
-    FROM empleado;
-)
-CREATE VIEW buys AS (
-    SELECT DNI, INTL, date, price, shippingAddr
-    FROM buys;
-)
-CREATE VIEW storage AS (
-    SELECT idPos, INTL, Quantity
-    FROM storage;
-)
+CREATE VIEW global_clients AS
+    SELECT *
+    FROM dblink('dbname=amazoon options=-csearch_path=',
+                'SELECT DNI, name, email, billingAddr FROM clients')
+    AS t1(DNI VARCHAR(9), name VARCHAR(50), email VARCHAR(255), billingAddr VARCHAR(100));
+
+CREATE VIEW buys AS
+    SELECT *
+    FROM dblink('dbname=amazoon options=-csearch_path=',
+                'SELECT DNI, INTL, date, price, shippingAddr FROM buys')
+    AS t1(DNI VARCHAR(9), INTL VARCHAR(36), date DATE, price FLOAT, shippingAddr VARCHAR(100));
+
+CREATE VIEW products AS
+    SELECT *
+    FROM
+    dblink('dbname=amazoon options=-csearch_path=',
+            'SELECT INTL, title, publicationDate FROM products')
+    UNION
+    dblink('dbname=manolo options=-csearch_path=',
+            'SELECT ISSN, titulo, fechaPublicacion, stock FROM revistas');
+
+SELECT dblink_exec('insert into POS values(''manolo'', ''physical'', ''Maria de Luna 3, Zaagoza, Spain'');');
+
+CREATE VIEW POS AS
+    SELECT *
+    FROM
+    dblink('dbname=amazoon options=-csearch_path=',
+    'SELECT id, type, address FROM POS');
+
+CREATE VIEW worker AS 
+    SELECT *
+    FROM dblink('dbname=manolo options=-csearch_path=',
+                'SELECT DNI, nombre, genero, salario, fechaIncorporacion, fechaSalida FROM empleado')
+    AS t1 (DNI VARCHAR(9), name VARCHAR(25), gender VARHCAR(6), salary DECIMAL, startingDate DATE, leavingDate DATE);
+
+CREATE VIEW sells AS 
+    SELECT * 
+    FROM dblink('dbname=manolo options=-csearch_path=',
+                'SELECT id, empleado, ISSN, fechaVenta, precioVenta, cantidad FROM vende')
+    AS t1 (id INTEGER, worker VARCHAR(9), INTL VARCHAR(36), date DATE, price DECIMAL, quantity INTEGER);
+
+CREATE VIEW storage AS
+    SELECT *
+    FROM
+    dblink('dbname=amazoon options=-csearch_path=',
+            'SELECT idPos, INTL, Quantity FROM storage')
+    UNION
+    dblink('dbname=manolo options=-csearch_path=',
+            'SELECT ISSN, stock FROM revista')
+    AS t1 (idPos VARCHAR(255), INTL VARCHAR(255), Quantity INT(11))
